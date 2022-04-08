54f70223c6de97c4a87482eff46ead29bd7b5d3b
salmanfaiz0 <64028304+salmanfaiz0@users.noreply.github.com>
2022-04-09 02:29:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+
+class Login(QDialog):
+    def __init__(self):
+        super(Login,self).__init__()
+        loadUi("page1.ui",self)
+        self.submit_button.clicked.connect(self.loginfunction)   ##ith submit button##
+        self.password.setEchoMode(QtWidgets.QLineEdit.Password)   ## ith phonebumber ##
+        self.createaccbutton.clicked.connect(self.gotocreate)     ## adutha finger ileak ##
+
+
+
+    def loginfunction(self):
+     password = self.password.text()
+     print("Successfully logged in with email: ", password)    ## output on terminal ##
+
+
+    def gotocreate(self):
+        createacc=CreateAcc()
+        widget.addWidget(createacc)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class CreateAcc(QDialog):
+    def __init__(self):
+        super(CreateAcc,self).__init__()
+        loadUi("page2.ui",self)
+        self.pushButton_3.clicked.connect(self.gotocreate)
+
+    def gotocreate(self):
+            createaccs = CreateAccs()
+            widget.addWidget(createaccs)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class CreateAccs(QDialog):
+    def __init__(self):
+        super(CreateAccs,self).__init__()
+        loadUi("page1",self)
+
+
+
+
+
+
+
+
+
+app=QApplication(sys.argv)
+mainwindow=Login()
+widget=QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedWidth(800)
+widget.setFixedHeight(480)
+widget.show()
+app.exec_()
